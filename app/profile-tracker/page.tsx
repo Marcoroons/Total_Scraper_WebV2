464c9f2488d05f731bb0575a1bb0f98e6b3dfc73
Marcoroons <88446572+Marcoroons@users.noreply.github.com>
2026-06-19 09:51:28 +0700

    scaffold: add app/profile-tracker/page.tsx

---
+++ app/profile-tracker/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfileTrackerPage() {
+  return (
+    <div className="p-2">
+      <h1 className="text-2xl font-bold text-gray-900">Profile Tracker</h1>
+    </div>
+  );
+}
